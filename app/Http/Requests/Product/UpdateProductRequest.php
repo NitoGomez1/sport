0633362dcf02b19991b15d4dbfe7b938c874713c
subsider <ccsanroman@gmail.com>
2018-12-13 09:23:05 +0100

    Update validation request for product

--- app/Http/Requests/Product/UpdateProductRequest.php
+++ app/Http/Requests/Product/UpdateProductRequest.php
@@ -4,8 +4,7 @@ namespace App\Http\Requests\Product;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateProductRequest extends FormRequest
-{
+class UpdateProductRequest extends FormRequest {
     /**
      * Determine if the user is authorized to make this request.
      *
